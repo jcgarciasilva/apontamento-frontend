fbf4db160ccea2e3c5db613ed183698e786a3406
Joao Carlos Garcia <jgarciasilva@gmail.com>
2020-03-25 11:57:25 -0300

    user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,35 +10,37 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ApontamentoModule } from './apontamento/apontamento.module';
 import { CadastroModule } from './cadastro/cadastro.module';
-import { UserModule } from './user/user.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ReportComponent } from './report/report.component';
 import { MenuNavComponent } from './menu-nav/menu-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    PageNotFoundComponent,
-    ReportComponent,
-    MenuNavComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MaterialModule,
-    BrowserAnimationsModule,
-    FlexLayoutModule,
-    FormsModule,
-    ReactiveFormsModule,
-    ApontamentoModule,
-    CadastroModule,
-    UserModule,
-    LayoutModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      PageNotFoundComponent,
+      ReportComponent,
+      MenuNavComponent,
+      UserComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      MaterialModule,
+      BrowserAnimationsModule,
+      FlexLayoutModule,
+      FormsModule,
+      ReactiveFormsModule,
+      ApontamentoModule,
+      CadastroModule,
+      LayoutModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
