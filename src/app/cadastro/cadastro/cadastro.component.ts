2fb78cbd4c5fcbfaae4d175035b2d825054ba591
Joao Carlos Garcia <jgarciasilva@gmail.com>
2020-03-27 15:41:08 -0300

    router

--- src/app/cadastro/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro/cadastro.component.ts
@@ -8,9 +8,9 @@ import { Router } from '@angular/router';
 
   <nav mat-tab-nav-bar>
     <a mat-tab-link routerLink="/cadastro/clients" routerLinkActive>Clientes</a>
-    <a mat-tab-link [routerLink="/cadastro/project" routerLinkActive>Projetos</a>
-    <a mat-tab-link [routerLink="/cadastro/service" routerLinkActive>Serviços</a>
-    <a mat-tab-link [routerLink="/cadastro/paramter" routerLinkActive>Parâmetros</a>
+    <a mat-tab-link routerLink="/cadastro/project" routerLinkActive>Projetos</a>
+    <a mat-tab-link routerLink="/cadastro/service" routerLinkActive>Serviços</a>
+    <a mat-tab-link routerLink="/cadastro/paramter" routerLinkActive>Parâmetros</a>
 </nav>
   <router-outlet></router-outlet>
   `,
