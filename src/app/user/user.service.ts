8626d32c6e37d05205dc174f8da097b8ba8a4035
João Garcia <jcgarciasilva@gmail.com>
2021-05-18 17:34:13 +0000

    add user service

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -5,24 +5,22 @@ import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { unescapeIdentifier } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService extends CacheService {
+export class UserService {
 
 
-  constructor(private angularFirestore: AngularFirestore, private angularFirebaseAuth: AngularFireAuth,) {
-    super();
+  constructor(private angularFirestore: AngularFirestore, private angularFirebaseAuth: AngularFireAuth,) { }
 
-  }
-
-  getUser(uid) {
-    return this.angularFirestore.collection<User>('users', q => q.where('uid', '==', uid)).get();
+  getUser(uid: string) {
+    return this.angularFirestore.collection<User>('users').doc(uid).get();
   }
 
   update(user: User) {
-    const userRef: AngularFirestoreDocument<User> = this.angularFirestore.doc(`users/${user.uid}`);
+    const userRef = this.angularFirestore.collection<User>('users').doc(user.uid);
     userRef.set(JSON.parse(JSON.stringify(user)), { merge: true });
   }
 
@@ -31,7 +29,7 @@ export class UserService extends CacheService {
     this.angularFirebaseAuth.createUserWithEmailAndPassword(user.email, 'oportuna')
       .then((result) => {
         user.uid = result.user.uid;
-        this.angularFirestore.collection('users').add(user);
+        this.angularFirestore.collection('users').doc(result.user.uid).set(user);
         result.user.sendEmailVerification();
       }
       ).catch(
